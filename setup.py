0da63e19958ca7eb8fc4a866a8ecfdcfce302c7a
Jiajun <jiajunb@umich.edu>
2020-06-24 10:53:11 -0400

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Neural-based Dimension Reduction Techniques for Nearest Neighbor Preservation.',
+    author='Jiajun Bao',
+    license='',
+)
