ebd46dc0b6b9eb399fd5a2f69509941b83bebcc4
Jiajun <jiajunb@umich.edu>
2020-07-04 23:34:08 -0400

    debug: calculate nearest neighbor in batches

--- models/DenseNetork/loss.py
+++ models/DenseNetork/loss.py
@@ -32,6 +32,7 @@ def nearest_neighbors(x, top_k):
             sorted_dist, indices = torch.sort(dist, dim=1, descending=False)
             batch_ground_min_dist_square = sorted_dist[:, 1]  # the 0-th column is the distance to oneself
             batch_topk_neighbors = indices[:, 1:1 + top_k]
+            print(batch_ground_min_dist_square.shape, batch_topk_neighbors.shape)
             topk_neighbors_list.append(batch_topk_neighbors)
             ground_min_dist_square_list.append(batch_ground_min_dist_square)
         ground_min_dist_square = torch.cat(ground_min_dist_square_list, dim=0)
