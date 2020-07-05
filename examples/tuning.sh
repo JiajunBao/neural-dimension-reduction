177392f390a504b66b2384606ae288e4e7e14bc3
Jiajun <jiajunb@umich.edu>
2020-07-05 17:35:13 -0400

    hidden layer dimensions as arguments

--- examples/tuning.sh
+++ examples/tuning.sh
@@ -2,15 +2,18 @@
 rm data/train.pth.tar
 rm data/dev.pth.tar
 
+TOP_K=20
+LR=1e-3
+
 python examples/train.py \
 --input_dir data \
---output_dir checkpoints/200-100-50-25-20/top100/1e-3 \
---learning_rate 1e-3 \
+--output_dir checkpoints/1/top${TOP_K}/${LR} \
+--learning_rate ${LR} \
 --n_epoch 600 \
 --per_gpu_batch_size 90000 \
 --num_eval_per_epoch 2 \
 --weight_decay 1e-5 \
---top_k 20 \
+--top_k ${TOP_K} \
 --hidden_dims_list [200, 100, 50, 25, 20]
 
 # rm data/train.pth.tar
