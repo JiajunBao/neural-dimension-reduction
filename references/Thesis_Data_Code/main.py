cc8976b8b0fb20f51e6cf85960a29126c77308b6
Jiajun <jiajunb@umich.edu>
2020-07-04 21:29:49 -0400

    numpy for cdist

--- references/Thesis_Data_Code/main.py
+++ references/Thesis_Data_Code/main.py
@@ -65,8 +65,8 @@ if __name__ == '__main__':
                     ]
     main(kwargs_list1)
 
-    kwargs_list1 = [{'input-size':90000,'test-size':1000,'hidden-dims':[500, 100, 20, 20, 20, 20, 20],'output-dimension':10,'lambda':1,'epochs':20,
-                    'to_plot':True,'learning_rate':0.01,'momentum':.9,'data':'artificial3_10_200','filename_prefix':'500_100_5*20'}
-                    ]
-    main(kwargs_list1)
+    # kwargs_list1 = [{'input-size':90000,'test-size':1000,'hidden-dims':[500, 100, 20, 20, 20, 20, 20],'output-dimension':10,'lambda':1,'epochs':20,
+    #                 'to_plot':True,'learning_rate':0.01,'momentum':.9,'data':'artificial3_10_200','filename_prefix':'500_100_5*20'}
+    #                 ]
+    # main(kwargs_list1)
 
