e911c24ccea46ffb941f73bafe91f56ff99eb19e
Jiajun <jiajunb@umich.edu>
2020-07-05 16:12:17 -0400

    nearest neighbors completed

--- examples/train.py
+++ examples/train.py
@@ -7,15 +7,16 @@ def train():
     hidden_dims_list = [200, 100, 50, 25, 20]
     dim_out = 20
     model = Net.from_scratch(dim_in, hidden_dims_list, dim_out)
+    args = Solver.get_solver_arguments()
     solver = Solver.from_scratch(model,
-                                 input_dir=Path('data'),
-                                 output_dir=Path('checkpoints/1e-3'),
-                                 learning_rate=1e-3,
-                                 n_epoch=1000,
-                                 per_gpu_batch_size=90000,
-                                 weight_decay=1e-5,
-                                 seed=42)
-    solver.fit(num_eval_per_epoch=2)
+                                 input_dir=args.input_dir,
+                                 output_dir=args.output_dir,
+                                 learning_rate=args.learning_rate,
+                                 n_epoch=args.n_epoch,
+                                 per_gpu_batch_size=args.per_gpu_batch_size,
+                                 weight_decay=args.weight_decay,
+                                 seed=args.seed)
+    solver.fit(num_eval_per_epoch=args.num_eval_per_epoch)
 
 
 if __name__ == '__main__':
---
+++ examples/tuning.sh
@@ -0,0 +1,11 @@
+
+
+python examples/train.py \
+--input_dir data \
+--output_dir checkpoints/1e-3 \
+--learning_rate 1e-3 \
+--n_epoch 600 \
+--per_gpu_batch_size 90000 \
+--num_eval_per_epoch 2 \
+--weight_decay 1e-5
+
