9df781a9d484206d174e0fb286e2a89ba1dea76f
Jiajun <jiajunb@umich.edu>
2020-07-05 18:02:27 -0400

    completed hidden layer: loss of test

--- examples/tuning.sh
+++ examples/tuning.sh
@@ -1,20 +1,17 @@
-
-rm data/train.pth.tar
-rm data/dev.pth.tar
-
 TOP_K=20
-LR=1e-3
+LR=1e-4
+HIDDEN_DIMS=200-100-50-25-20
 
 python examples/train.py \
 --input_dir data \
 --output_dir checkpoints/1/top${TOP_K}/${LR} \
 --learning_rate ${LR} \
---n_epoch 600 \
+--n_epoch 1200 \
 --per_gpu_batch_size 90000 \
 --num_eval_per_epoch 2 \
 --weight_decay 1e-5 \
 --top_k ${TOP_K} \
---hidden_dims_list 200-100-50-25-20
+--hidden_dims_list ${HIDDEN_DIMS}
 
 # rm data/train.pth.tar
 # rm data/dev.pth.tar
