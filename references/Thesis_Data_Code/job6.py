fec6a171f82e56ba18fed57fe52d9d496f4fccd4
Jiajun <jiajunb@umich.edu>
2020-07-06 16:10:06 -0400

    thesis reproduction recheck

--- references/Thesis_Data_Code/job6.py
+++ references/Thesis_Data_Code/job6.py
@@ -25,3 +25,12 @@ kwargs_list1 = [
      'filename_prefix': 'thesis_3'}
     ]
 main(kwargs_list1)
+
+kwargs_list1 = [
+    {'input-size': 90000, 'test-size': 1000, 'hidden-dims': [5000, 1000, 200, 200, 200], 'output-dimension': 10, 'lambda': 1,
+     'epochs': 20,
+     'to_plot': True, 'learning_rate': 0.01, 'momentum': .9, 'data': 'artificial3_10_200',
+     'filename_prefix': 'thesis_3'}
+    ]
+main(kwargs_list1)
+
