159a3ac5c73d2231e131c72d1a66405dd5cf9192
Jiajun <jiajunb@umich.edu>
2020-07-21 08:58:09 -0400

    tsne score for 2000 points

--- examples/run_tsne.sh
+++ examples/run_tsne.sh
@@ -1,12 +1,13 @@
 
 python src/models/tSNE/tSNE.py \
 --input_path data/processed/sample/dev.csv \
---output_dir checkpoints/tsne/sample-test-set \
---dim_out 2 \
+--output_dir checkpoints/tsne/sample-dev-set \
+--dim_out 20 \
 --n_iter 4000
 
 
 python src/models/tSNE/tSNE.py \
 --input_path data/processed/sample/train.csv \
 --output_dir checkpoints/tsne/sample-train-set \
+--dim_out 20 \
 --n_iter 4000
