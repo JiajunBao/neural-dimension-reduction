3ecc3e8284c4fe91adfdab66a47329144c2abf62
Jiajun <jiajunb@umich.edu>
2020-07-23 19:29:09 -0400

    ResNet ResidualBottleneckBlock

--- examples/resnet_tuning.sh
+++ examples/resnet_tuning.sh
@@ -1,7 +1,25 @@
-TOP_K=2000
-LR=1e-3
+#TOP_K=2000
+#LR=1e-3
+#
+#for HIDDEN_DIMS in resnet32 resnet47
+#do
+#  python examples/train_resnet.py \
+#  --input_dir data/processed/sample \
+#  --output_dir checkpoints/sample/"${HIDDEN_DIMS}"/top${TOP_K}/${LR} \
+#  --learning_rate ${LR} \
+#  --n_epoch 2000 \
+#  --per_gpu_batch_size 90000 \
+#  --num_eval_per_epoch 2 \
+#  --weight_decay 1e-6 \
+#  --top_k ${TOP_K} \
+#  --hidden_dims_list "${HIDDEN_DIMS}" \
+#  --add_shortcut True
+#done
 
-for HIDDEN_DIMS in resnet32 resnet47
+TOP_K=2000
+HIDDEN_DIMS=resnet47
+
+for LR in 1e-5 2e-5 4e-5 8e-5 1.6e-4 3.2e-4 6.4e-4
 do
   python examples/train_resnet.py \
   --input_dir data/processed/sample \
@@ -15,22 +33,3 @@ do
   --hidden_dims_list "${HIDDEN_DIMS}" \
   --add_shortcut True
 done
-
-
-#LR=1e-3
-#MODEL_NAME=resnet32
-#TOP_K=2000
-#
-#rm data/processed/sample/train.insane.pth.tar
-#rm data/processed/sample/dev.insane.pth.tar
-#
-#python examples/train_resnet.py \
-#--input_dir data/processed/sample \
-#--output_dir checkpoints/sample/"${MODEL_NAME}"/top${TOP_K}/${LR} \
-#--learning_rate ${LR} \
-#--n_epoch 1200 \
-#--per_gpu_batch_size 90000 \
-#--num_eval_per_epoch 2 \
-#--weight_decay 1e-6 \
-#--top_k ${TOP_K} \
-#--config_name "${MODEL_NAME}"
