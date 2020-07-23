f0b5249bd75efdaa203e02c9e6f39357aa4f3d29
Jiajun <jiajunb@umich.edu>
2020-07-23 19:05:52 -0400

    ResNet ResidualBottleneckBlock

--- examples/resnet_tuning.sh
+++ examples/resnet_tuning.sh
@@ -7,7 +7,7 @@ do
   --input_dir data/processed/sample \
   --output_dir checkpoints/sample/"${HIDDEN_DIMS}"/top${TOP_K}/${LR} \
   --learning_rate ${LR} \
-  --n_epoch 1200 \
+  --n_epoch 2000 \
   --per_gpu_batch_size 90000 \
   --num_eval_per_epoch 2 \
   --weight_decay 1e-6 \
@@ -15,6 +15,7 @@ do
   --hidden_dims_list "${HIDDEN_DIMS}" \
   --add_shortcut True
 done
+
 
 #LR=1e-3
 #MODEL_NAME=resnet32
