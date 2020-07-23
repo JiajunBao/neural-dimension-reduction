d1f5241ec75ab4330f5ea75f647a8d6aef014496
Jiajun <jiajunb@umich.edu>
2020-07-23 17:46:35 -0400

    ResNet ResidualBottleneckBlock

--- examples/resnet_tuning.sh
+++ examples/resnet_tuning.sh
@@ -1,17 +1,35 @@
-LR=1e-3
-MODEL_NAME=resnet32
 TOP_K=2000
+LR=1e-3
 
-rm data/processed/sample/train.insane.pth.tar
-rm data/processed/sample/dev.insane.pth.tar
+for HIDDEN_DIMS in resnet32 resnet47
+do
+  python examples/train.py \
+  --input_dir data/processed/sample \
+  --output_dir checkpoints/sample/"${HIDDEN_DIMS}"/top${TOP_K}/${LR} \
+  --learning_rate ${LR} \
+  --n_epoch 1200 \
+  --per_gpu_batch_size 90000 \
+  --num_eval_per_epoch 2 \
+  --weight_decay 1e-6 \
+  --top_k ${TOP_K} \
+  --hidden_dims_list "${HIDDEN_DIMS}" \
+  --add_shortcut True
+done
 
-python examples/train_resnet.py \
---input_dir data/processed/sample \
---output_dir checkpoints/sample/"${MODEL_NAME}"/top${TOP_K}/${LR} \
---learning_rate ${LR} \
---n_epoch 1200 \
---per_gpu_batch_size 90000 \
---num_eval_per_epoch 2 \
---weight_decay 1e-6 \
---top_k ${TOP_K} \
---config_name "${MODEL_NAME}"
+#LR=1e-3
+#MODEL_NAME=resnet32
+#TOP_K=2000
+#
+#rm data/processed/sample/train.insane.pth.tar
+#rm data/processed/sample/dev.insane.pth.tar
+#
+#python examples/train_resnet.py \
+#--input_dir data/processed/sample \
+#--output_dir checkpoints/sample/"${MODEL_NAME}"/top${TOP_K}/${LR} \
+#--learning_rate ${LR} \
+#--n_epoch 1200 \
+#--per_gpu_batch_size 90000 \
+#--num_eval_per_epoch 2 \
+#--weight_decay 1e-6 \
+#--top_k ${TOP_K} \
+#--config_name "${MODEL_NAME}"
--- examples/train_resnet.py
+++ examples/train_resnet.py
@@ -1,25 +1,39 @@
 import json
 
-from src.models.INSANE.models import InsaneEncoder
-from src.models.utils.Trainer import InsaneTrainer
+from src.models.DenseNetwork.models import Net, Solver
+from src.models.ResNet.ResNet import get_resnet
+from pathlib import Path
 
 
 def train():
     with open('models/model_zoo.json', 'r') as istream:
         model_zoo = json.load(istream)
-    args = InsaneTrainer.get_solver_arguments()
-    encoder = InsaneEncoder.from_scratch(encoder_config=model_zoo[args.config_name],
-                                         top_k=args.top_k)
-    trainer = InsaneTrainer.from_scratch(model=encoder,
-                                         input_dir=args.input_dir,
-                                         output_dir=args.output_dir,
-                                         learning_rate=args.learning_rate,
-                                         n_epoch=args.n_epoch,
-                                         per_gpu_batch_size=args.per_gpu_batch_size,
-                                         weight_decay=args.weight_decay,
-                                         seed=args.seed,
-                                         top_k=args.top_k)
-    trainer.fit(num_eval_per_epoch=args.num_eval_per_epoch)
+    # args = InsaneTrainer.get_solver_arguments()
+    # encoder = InsaneEncoder.from_scratch(encoder_config=model_zoo[args.config_name],
+    #                                      top_k=args.top_k)
+    # trainer = InsaneTrainer.from_scratch(model=encoder,
+    #                                      input_dir=args.input_dir,
+    #                                      output_dir=args.output_dir,
+    #                                      learning_rate=args.learning_rate,
+    #                                      n_epoch=args.n_epoch,
+    #                                      per_gpu_batch_size=args.per_gpu_batch_size,
+    #                                      weight_decay=args.weight_decay,
+    #                                      seed=args.seed,
+    #                                      top_k=args.top_k)
+    # trainer.fit(num_eval_per_epoch=args.num_eval_per_epoch)
+    args = Solver.get_solver_arguments()
+    model = get_resnet(model_zoo[args.hidden_dims_list])
+
+    solver = Solver.from_scratch(model,
+                                 input_dir=args.input_dir,
+                                 output_dir=args.output_dir,
+                                 learning_rate=args.learning_rate,
+                                 n_epoch=args.n_epoch,
+                                 per_gpu_batch_size=args.per_gpu_batch_size,
+                                 weight_decay=args.weight_decay,
+                                 seed=args.seed,
+                                 top_k=args.top_k)
+    solver.fit(num_eval_per_epoch=args.num_eval_per_epoch)
 
 
 if __name__ == '__main__':
