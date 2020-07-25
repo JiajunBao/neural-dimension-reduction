25a1e6e06248395af811f4d9865f14d7dc33efbf
Jiajun <jiajunb@umich.edu>
2020-07-24 22:49:54 -0400

    tuning

--- examples/resnet_tuning.sh
+++ examples/resnet_tuning.sh
@@ -40,18 +40,20 @@ set -x
 
 TOP_K=2000
 
-LR=4e-5
 for HIDDEN_DIMS in resnet7 resnet17 resnet27 resnet37
 do
-  python examples/train_resnet.py \
-  --input_dir data/processed/sample \
-  --output_dir checkpoints/sample/"${HIDDEN_DIMS}"/top${TOP_K}/${LR} \
-  --learning_rate ${LR} \
-  --n_epoch 2000 \
-  --per_gpu_batch_size 90000 \
-  --num_eval_per_epoch 2 \
-  --weight_decay 1e-6 \
-  --top_k ${TOP_K} \
-  --hidden_dims_list "${HIDDEN_DIMS}" \
-  --add_shortcut True
+  for LR in 1e-5 2e-5 8e-5 1.6e-4 3.2e-4 6.4e-4
+  do
+    python examples/train_resnet.py \
+    --input_dir data/processed/sample \
+    --output_dir checkpoints/sample/"${HIDDEN_DIMS}"/top${TOP_K}/${LR} \
+    --learning_rate ${LR} \
+    --n_epoch 2000 \
+    --per_gpu_batch_size 90000 \
+    --num_eval_per_epoch 2 \
+    --weight_decay 1e-6 \
+    --top_k ${TOP_K} \
+    --hidden_dims_list "${HIDDEN_DIMS}" \
+    --add_shortcut True
+  done
 done
