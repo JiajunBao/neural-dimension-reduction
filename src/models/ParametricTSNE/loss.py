5462fe559f35ccb848658481df40f4f30ce3f2dd
Jiajun <jiajunb@umich.edu>
2020-07-31 11:27:45 -0400

    param tsne

--- src/models/ParametricTSNE/loss.py
+++ src/models/ParametricTSNE/loss.py
@@ -1,48 +1,95 @@
-import torch
-from torch.nn import functional as F
-# from tqdm.auto import tqdm
+import numpy as np
 
 
-def pairwise_distance(x):
-    """
-    calculate the nearest neighbors of x, return the
-    :param x: for matrix to calculate nearest neighbor
-    :return:
-        dist: pairwise distances
-    """
-    batch_size = 2000
-    if x.shape[0] * x.shape[1] < batch_size * 200:  # direct computes the whole matrix
-        dist = torch.cdist(x1=x, x2=x, p=2)  # (n, n)
-    else:  # calculate the nearest neighbors in batches
-        num_iter = x.shape[0] // batch_size + 1
-        dist = list()
-        for i in torch.arange(num_iter):
-            batch_x = x[i * batch_size: (i + 1) * batch_size, :]
-            dist.append(torch.cdist(x1=batch_x, x2=x, p=2))  # (n, n)
-        dist = torch.cat(dist, dim=0)
-    return dist
+def Hbeta(D, beta):
+    P = np.exp(-D * beta)
+    sumP = np.sum(P)
+    H = np.log(sumP) + beta * np.sum(np.multiply(D, P)) / sumP
+    P = P / sumP
+    return H, P
 
 
-def softmin_probability(xdist_batch, sigma=30):
-    n = xdist_batch.shape[0]
-    idx = torch.arange(n)
-    d = -xdist_batch.square() / 2 / (sigma ** 2)
-    d = d - torch.mean(d)
-    p = d.exp()
-    p[idx, idx] = 0
-    p = p / p.sum(dim=1, keepdim=True)
-    p[idx, idx] = 0
-    return p
+def x2p(X, u=15, tol=1e-4, print_iter=500, max_tries=50, verbose=0):
+    # Initialize some variables
+    n = X.shape[0]  # number of instances
+    P = np.zeros((n, n))  # empty probability matrix
+    beta = np.ones(n)  # empty precision vector
+    logU = np.log(u)  # log of perplexity (= entropy)
+
+    # Compute pairwise distances
+    if verbose > 0: print('Computing pairwise distances...')
+    sum_X = np.sum(np.square(X), axis=1)
+    # note: translating sum_X' from matlab to numpy means using reshape to add a dimension
+    D = sum_X + sum_X[:, None] + -2 * X.dot(X.T)
+
+    # Run over all datapoints
+    if verbose > 0: print('Computing P-values...')
+    for i in range(n):
+
+        if verbose > 1 and print_iter and i % print_iter == 0:
+            print('Computed P-values {} of {} datapoints...'.format(i, n))
+
+        # Set minimum and maximum values for precision
+        betamin = float('-inf')
+        betamax = float('+inf')
+
+        # Compute the Gaussian kernel and entropy for the current precision
+        indices = np.concatenate((np.arange(0, i), np.arange(i + 1, n)))
+        Di = D[i, indices]
+        H, thisP = Hbeta(Di, beta[i])
+
+        # Evaluate whether the perplexity is within tolerance
+        Hdiff = H - logU
+        tries = 0
+        while abs(Hdiff) > tol and tries < max_tries:
+
+            # If not, increase or decrease precision
+            if Hdiff > 0:
+                betamin = beta[i]
+                if np.isinf(betamax):
+                    beta[i] *= 2
+                else:
+                    beta[i] = (beta[i] + betamax) / 2
+            else:
+                betamax = beta[i]
+                if np.isinf(betamin):
+                    beta[i] /= 2
+                else:
+                    beta[i] = (beta[i] + betamin) / 2
+
+            # Recompute the values
+            H, thisP = Hbeta(Di, beta[i])
+            Hdiff = H - logU
+            tries += 1
+
+        # Set the final row of P
+        P[i, indices] = thisP
+
+    if verbose > 0:
+        print('Mean value of sigma: {}'.format(np.mean(np.sqrt(1 / beta))))
+        print('Minimum value of sigma: {}'.format(np.min(np.sqrt(1 / beta))))
+        print('Maximum value of sigma: {}'.format(np.max(np.sqrt(1 / beta))))
+
+    return P, beta
 
 
-def calculate_perplexity(prob):
-    n = prob.shape[0]
-    idx = torch.arange(n)
-    entropy = torch.log2(prob) * prob
-    entropy[idx, idx] = 0
-    hp = -torch.sum(entropy, dim=1)
-    return 2 ** hp
+def compute_joint_probabilities(samples, batch_size=5000, d=2, perplexity=30, tol=1e-5, verbose=0):
+    v = d - 1
 
+    # Initialize some variables
+    n = samples.shape[0]
+    batch_size = min(batch_size, n)
 
-def get_output_probability(y, alpha):
-    pass
+    # Precompute joint probabilities for all batches
+    if verbose > 0: print('Precomputing P-values...')
+    batch_count = int(n / batch_size)
+    P = np.zeros((batch_count, batch_size, batch_size))
+    for i, start in enumerate(range(0, n - batch_size + 1, batch_size)):
+        curX = samples[start:start + batch_size]  # select batch
+        P[i], beta = x2p(curX, perplexity, tol, verbose=verbose)  # compute affinities using fixed perplexity
+        P[i][np.isnan(P[i])] = 0  # make sure we don't have NaN's
+        P[i] = (P[i] + P[i].T)  # / 2                             # make symmetric
+        P[i] = P[i] / P[i].sum()  # obtain estimation of joint probabilities
+        P[i] = np.maximum(P[i], np.finfo(P[i].dtype).eps)
+
+    return P
