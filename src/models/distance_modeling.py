efe24bf2c5241f5e382b4dc7c0995fee1f26c090
JiajunBao <jiajunb@umich.edu>
2020-08-18 12:08:07 +0000

    update distance modeling: ok

--- src/models/distance_modeling.py
+++ src/models/distance_modeling.py
@@ -16,7 +16,7 @@ def far_func(sorted_dist: torch.tensor, indices: torch.tensor):
 
 def calculate_distance(x, far_fn):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    batch_size = 16
+    batch_size = 512
     x_device = x.to(device)
     if x.shape[0] * x.shape[1] < batch_size * 200:  # direct computes the whole matrix
         # TODO: we first assume memory fits in memory. Later, we can process the data in batches.
@@ -80,9 +80,9 @@ class SurveyorDataSet(Dataset):
         self.q = q
 
     @classmethod
-    def from_df(cls, path_to_dataframe):
+    def from_df(cls, path_to_dataframe, far_fn=far_func):
         data = torch.from_numpy(pd.read_csv(path_to_dataframe, header=None).to_numpy()).to(torch.float32)
-        pairs, labels, close_distance, far_distance = make_pairs(data, far_func)
+        pairs, labels, close_distance, far_distance = make_pairs(data, far_fn)
         q = thesis_input_inverse_similarity(data[pairs[:, 0]],
                                             data[pairs[:, 1]],
                                             close_distance[pairs[:, 0]],
@@ -180,3 +180,47 @@ def thesis_kl_div_add_mse_loss(p, q, lam=1):
     :return: torch.tensor of the shape (,)
     """
     return torch.sum(p * torch.log(p / q)) + lam * torch.sum((p - q) ** 2)
+
+class RetrieveSystem(object):
+    def __init__(self, distance_measure):
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        distance_measure = distance_measure.to(self.device)
+        self.distance_measure = distance_measure
+
+    def retrieve_query(self, query, ignore_idx, x_embedded, x_idx, topk=20):
+        query_device = query.view(1, -1).to(self.device)
+        cls_distances = list()
+        p_distances = list()
+        with torch.no_grad():
+            for i, x in zip(x_idx, x_embedded):
+                if ignore_idx is not None and i == ignore_idx:
+                    continue
+                x_device = x.view(1, -1).to(self.device)
+                logits, p = self.distance_measure.decode_batch(query_device, x_device)
+                cls_distances.append(logits[:, 1].item())
+                p_distances.append(p.item())
+        cls_distances = torch.tensor(cls_distances)
+        p_distances = torch.tensor(p_distances)
+        _, cls_nn_idx = cls_distances.sort(descending=True)
+        _, p_nn_idx = p_distances.sort(descending=True)
+        return cls_nn_idx[:topk], p_nn_idx[:topk]
+
+    def retrieve_corpus(self, corpus, block_list, database):
+        cls_pred_nn_top, p_distances_nn_top = list(), list()
+        x_idx = range(database.shape[0])
+        for ignore_idx, query in tqdm(zip(block_list, corpus), total=len(block_list), desc='retrieve each query'):
+            cls_distances, p_distances = self.retrieve_query(query, ignore_idx, database, x_idx, 20)
+            cls_pred_nn_top.append(cls_distances.view(1, -1))
+            p_distances_nn_top.append(p_distances.view(1, -1))
+        cls_pred_nn_top = torch.cat(cls_pred_nn_top, dim=0)
+        p_distances_nn_top = torch.cat(p_distances_nn_top, dim=0)
+        return cls_pred_nn_top, p_distances_nn_top
+
+    def recall(self, pred, gold, at_n=None):
+        results = dict()
+        if at_n is None:
+            at_n = [1, 5, 10, 20]
+        for n in at_n:
+            recall = float((pred[:, :n] == gold.view(-1, 1)).sum().item()) / len(gold)
+            results[f'recall@{n}'] = recall
+        return results
