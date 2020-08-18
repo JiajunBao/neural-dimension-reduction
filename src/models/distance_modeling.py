76210633f2ffa42a7f3b06ffd6f11ae3b5a59e67
JiajunBao <jiajunb@umich.edu>
2020-08-18 14:08:00 +0000

    multi neg

--- src/models/distance_modeling.py
+++ src/models/distance_modeling.py
@@ -64,15 +64,15 @@ def make_pairs(x, close_fn, far_fn):
     anchor_idx, close_idx, far_idx, close_distance, far_distance = calculate_distance(x, close_fn, far_fn)
     
     n, r = close_idx.shape
-    anchor_idx_flatten = anchor_idx.view(-1, 1).expand(-1, r).view(-1, 1)  # (n * r, )
-    close_idx = close_idx.view(-1, 1)  # (n * r, 1)
+    anchor_idx_flatten = anchor_idx.reshape(-1, 1).expand(-1, r).reshape(-1, 1)  # (n * r, )
+    close_idx = close_idx.reshape(-1, 1)  # (n * r, 1)
 
     positive_pairs = torch.cat((anchor_idx_flatten, close_idx), dim=1)  # (n, 2)
     positive_labels = torch.ones(n, dtype=torch.int64)  # (n, )
     
     n, r = far_idx.shape
-    far_idx = far_idx.view(-1, 1)  # (n * r, )
-    anchor_idx_flatten = anchor_idx.view(-1, 1).expand(-1, r).view(-1, 1)  # (n * r, )
+    far_idx = far_idx.reshape(-1, 1)  # (n * r, )
+    anchor_idx_flatten = anchor_idx.reshape(-1, 1).expand(-1, r).reshape(-1, 1)  # (n * r, )
     negative_pairs = torch.cat((anchor_idx_flatten, far_idx), dim=1)  # (n * r, 2)
     negative_labels = torch.zeros(n * r, dtype=torch.int64)  # (n * r, )
     pairs = torch.cat((positive_pairs, negative_pairs), dim=0)
@@ -91,11 +91,16 @@ class SurveyorDataSet(Dataset):
     def from_df(cls, path_to_dataframe, close_fn=close_func, far_fn=far_func):
         data = torch.from_numpy(pd.read_csv(path_to_dataframe, header=None).to_numpy()).to(torch.float32)
         pairs, labels, close_distance, far_distance = make_pairs(data, close_fn, far_fn)
-        assert close_distance[pairs[:, 0]].shape[1] == 1
-        q = thesis_input_inverse_similarity(data[pairs[:, 0]],
-                                            data[pairs[:, 1]],
-                                            close_distance[pairs[:, 0]].view(-1),
-                                            close_distance[pairs[:, 1]].view(-1))
+        if close_distance[pairs[:, 0]].shape[1] == 1:
+            q = thesis_input_inverse_similarity(data[pairs[:, 0]],
+                                                data[pairs[:, 1]],
+                                                close_distance[pairs[:, 0]].reshape(-1),
+                                                close_distance[pairs[:, 1]].reshape(-1))
+        else:
+            q = thesis_input_inverse_similarity(data[pairs[:, 0]],
+                                                data[pairs[:, 1]],
+                                                close_distance[pairs[:, 0], 0].reshape(-1),
+                                                close_distance[pairs[:, 1], 0].reshape(-1))
         return cls(data, pairs, labels, q)
 
     @classmethod
@@ -197,14 +202,14 @@ class RetrieveSystem(object):
         self.distance_measure = distance_measure
 
     def retrieve_query(self, query, ignore_idx, x_embedded, x_idx, topk=20):
-        query_device = query.view(1, -1).to(self.device)
+        query_device = query.reshape(1, -1).to(self.device)
         cls_distances = list()
         p_distances = list()
         with torch.no_grad():
             for i, x in zip(x_idx, x_embedded):
                 if ignore_idx is not None and i == ignore_idx:
                     continue
-                x_device = x.view(1, -1).to(self.device)
+                x_device = x.reshape(1, -1).to(self.device)
                 logits, p = self.distance_measure.decode_batch(query_device, x_device)
                 cls_distances.append(logits[:, 1].item())
                 p_distances.append(p.item())
@@ -219,8 +224,8 @@ class RetrieveSystem(object):
         x_idx = range(database.shape[0])
         for ignore_idx, query in tqdm(zip(block_list, corpus), total=len(block_list), desc='retrieve each query'):
             cls_distances, p_distances = self.retrieve_query(query, ignore_idx, database, x_idx, 20)
-            cls_pred_nn_top.append(cls_distances.view(1, -1))
-            p_distances_nn_top.append(p_distances.view(1, -1))
+            cls_pred_nn_top.append(cls_distances.reshape(1, -1))
+            p_distances_nn_top.append(p_distances.reshape(1, -1))
         cls_pred_nn_top = torch.cat(cls_pred_nn_top, dim=0)
         p_distances_nn_top = torch.cat(p_distances_nn_top, dim=0)
         return cls_pred_nn_top, p_distances_nn_top
@@ -230,6 +235,6 @@ class RetrieveSystem(object):
         if at_n is None:
             at_n = [1, 5, 10, 20]
         for n in at_n:
-            recall = float((pred[:, :n] == gold.view(-1, 1)).sum().item()) / len(gold)
+            recall = float((pred[:, :n] == gold.reshape(-1, 1)).sum().item()) / len(gold)
             results[f'recall@{n}'] = recall
         return results
