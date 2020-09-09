4486c4b8195dfc0821984739b49679c247f3cf9a
Jiajun <jiajunb@umich.edu>
2020-09-09 10:26:19 -0400

    get raw tensor get dataset

--- src/models/level_kv_div/datasets.py
+++ src/models/level_kv_div/datasets.py
@@ -3,6 +3,11 @@ from torch.utils.data import TensorDataset
 import pandas as pd
 
 
-def get_dataset(path):
-    t = torch.from_numpy(pd.read_csv(path, header=None).to_numpy())
-    return TensorDataset(t)
+def get_dataset(raw_path, sim_path):
+    t = torch.from_numpy(pd.read_csv(raw_path, header=None).to_numpy())
+    sim = torch.load(sim_path, 'cpu')
+    return TensorDataset(t, sim)
+
+
+def get_raw_tensor(path):
+    return torch.from_numpy(pd.read_csv(path, header=None).to_numpy())
