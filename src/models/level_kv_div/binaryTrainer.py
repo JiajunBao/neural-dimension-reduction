473c50029748cea1faaacb4726ebcbf1afe7d835
JiajunBao <jiajunb@umich.edu>
2020-09-09 21:26:47 +0000

    updated

--- src/models/level_kv_div/binaryTrainer.py
+++ src/models/level_kv_div/binaryTrainer.py
@@ -3,9 +3,9 @@ from torch import nn
 from torch.utils.data import Dataset
 import copy
 # from torch.nn import KLDivLoss
-
+import random
 import pandas as pd
-
+random.seed(35)
 
 def get_dataset(x_path, label_path):
     return SparseDataset(x_path, label_path)
@@ -17,11 +17,27 @@ class SparseDataset(Dataset):
         label_matrix = torch.load(label_path, 'cpu').float()
         assert x.shape[0] == label_matrix.shape[0], f'inconsistent size {x.shape[0]} vs {label_matrix.shape[0]} .'
         data = list()
+        count1, count0, countn1 = 0, 0, 0
         for i, vec in enumerate(label_matrix):
+            tmp_pos_data, tmp_neg_data = list(), list()
             for j, label in enumerate(vec):
-                data.append((i, j, label))
+                if label.item() == 1.:
+                    tmp_neg_data.append((i, j, label.item(),))
+                    count1 += 1
+                elif label.item() == 0.:
+                    tmp_pos_data.append((i, j, label.item(),))
+                    count0 += 1
+                elif label.item() == -1.:
+                    tmp_pos_data.append((i, j, label.item(),))
+                    countn1 += 1
+                else:
+                    raise NotImplemented
+            random.shuffle(tmp_neg_data)
+            data += tmp_neg_data[:len(tmp_pos_data)] + tmp_pos_data
         self.data = data
         self.x = x
+        print(f'{label_matrix.shape[0]} points {len(self.data)} data')
+        print(f'mutual negihbors: {countn1}, one-direction neighbor: {count0}, not neighbor: {count1}')
 
     def __len__(self):
         return len(self.data)
@@ -43,9 +59,9 @@ class TriMarginLoss:
         l2 = (y - 1) * (y - 1) * (y + 1) * torch.max(dist - self.m2, self.m3 - dist)
         l3 = (y + 1) * (y - 0.5) * torch.clamp(self.m4 - dist, min=0)
         loss = l1 + l2 + l3
-        if self.reduction == 'mean':
-            return torch.mean(loss)
-        return loss, dist
+        if self.reduction == 'sum':
+            return loss.sum(), dist
+        return loss.mean(), dist
 
     def get_distance(self, output1: torch.tensor, output2: torch.tensor):
         return torch.sum((output1 - output2) ** 2, dim=1)
@@ -56,7 +72,8 @@ def train_one_epoch(train_loader, model, optimizer, verbose, device):
     model.train()
     criterion = TriMarginLoss(1, 3, 4, 6, 'mean')
     train_margin_loss = 0.
-    # pred_list = list()
+    pred_list = list()
+    label_list = list()
     train_correct_pred = 0
     for i, batch in enumerate(train_loader):
         x1, x2, label = batch
@@ -65,9 +82,10 @@ def train_one_epoch(train_loader, model, optimizer, verbose, device):
         loss, dist = criterion.forward(output1, output2, label.to(device))
 
         dist[dist <= 1] = -1
-        dist[dist >= 3 & dist <= 4] = 0
+        dist[(dist >= 3) & (dist <= 4)] = 0
         dist[dist >= 6] = 1
-        # pred_list.append(dist.cpu())
+        pred_list.append(dist.cpu())
+        label_list.append(label.cpu())
         train_correct_pred += (dist == label.to(device)).sum().item()
 
         model.zero_grad()  # reset gradient
@@ -76,15 +94,17 @@ def train_one_epoch(train_loader, model, optimizer, verbose, device):
         train_margin_loss += loss.item()
         if verbose and i % 20 == 0:
             print(f'training loss: {train_margin_loss / (i + 1):.4f}')
-    # pred = torch.cat(pred_list, dim=0)
-    return train_margin_loss / len(train_loader), train_correct_pred / len(train_loader)
+    pred = torch.cat(pred_list, dim=0)
+    gold = torch.cat(label_list, dim=0)
+    return train_margin_loss / len(train_loader.dataset), (train_correct_pred / len(train_loader.dataset), pred, gold,)
 
 
 def val_one_epoch(val_loader, model, device):
     model.eval()
     criterion = TriMarginLoss(1, 3, 4, 6, 'mean')
     val_margin_loss = 0.
-    # pred_list = list()
+    pred_list = list()
+    label_list = list()
     val_correct_pred = 0
     with torch.no_grad():
         for i, batch in enumerate(val_loader):
@@ -93,21 +113,23 @@ def val_one_epoch(val_loader, model, device):
             output1, output2 = model(x1_device, x2_device)
             loss, dist = criterion.forward(output1, output2, label.to(device))
             dist[dist <= 1] = -1
-            dist[dist >= 3 & dist <= 4] = 0
+            dist[(dist >= 3) & (dist <= 4)] = 0
             dist[dist >= 6] = 1
             val_correct_pred += (dist == label.to(device)).sum().item()
-            # pred_list.append(dist.cpu())
+            pred_list.append(dist.cpu())
+            label_list.append(label.cpu())
             val_margin_loss += loss.item()
-    # pred = torch.cat(pred_list, dim=0)
-    return val_margin_loss / len(val_loader), val_correct_pred / len(val_loader)
+    pred = torch.cat(pred_list, dim=0)
+    gold = torch.cat(label_list, dim=0)
+    return val_margin_loss / len(val_loader.dataset), (val_correct_pred / len(val_loader.dataset), pred, gold,)
 
 
 def train_with_eval(train_loader, val_loader, model, optimizer, num_epoches, log_epoch, verbose, device):
     best_model = None
     best_avg_val_margin_loss = float('inf')
     for epoch_idx in range(1, num_epoches + 1):
-        avg_train_loss, train_accuracy = train_one_epoch(train_loader, model, optimizer, False, device)
-        avg_val_margin_loss, val_accuracy = val_one_epoch(val_loader, model, device)
+        avg_train_loss, (train_accuracy, train_pred, train_gold) = train_one_epoch(train_loader, model, optimizer, False, device)
+        avg_val_margin_loss, (val_accuracy,  val_pred, val_gold) = val_one_epoch(val_loader, model, device)
         if avg_val_margin_loss > best_avg_val_margin_loss:
             best_avg_val_margin_loss = avg_val_margin_loss
             best_model = copy.deepcopy(model.cpu())
