2e5f19a82d5ecbeb3b4f121e0f81e048e0eeb454
Jiajun <jiajunb@umich.edu>
2020-09-12 08:46:26 -0400

    linear search eval

--- src/models/level_kv_div/binaryTrainer.py
+++ src/models/level_kv_div/binaryTrainer.py
@@ -97,7 +97,7 @@ class SparseDataset(Dataset):
         return self.x[id1], self.x[id2], label
 
 
-def evaluate_results(x, model, k, loss_param):
+def evaluate_results(x, model, k, loss_param, cache_dist):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     batch_size = 512
     model = model.to(device)
@@ -116,6 +116,10 @@ def evaluate_results(x, model, k, loss_param):
     data_loader = DataLoader(TensorDataset(x, embedded_x), batch_size=batch_size, pin_memory=True, shuffle=False)
     margin_measure_confusion = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
     linear_search_confusion = {'tp': 0, 'tn': 0, 'fp': 0, 'fn': 0}
+    if cache_dist:
+        pred_dist_list = list()
+    else:
+        pred_dist_list = None
     for batch_x, batch_embedded_x in tqdm(data_loader):
         gold_dist = torch.cdist(batch_x.to(device), x.to(device), p=2).cpu()
         gold_sorted_dist, gold_indices = torch.sort(gold_dist, dim=1, descending=False)
@@ -145,6 +149,10 @@ def evaluate_results(x, model, k, loss_param):
         margin_measure_confusion['tn'] += (binary_gold[margin_measure_pred == 0] == 0).sum().item()
         margin_measure_confusion['fp'] += (binary_gold[margin_measure_pred == 1] == 0).sum().item()
         margin_measure_confusion['fn'] += (binary_gold[margin_measure_pred == 0] == 1).sum().item()
+
+        if cache_dist:
+            pred_dist_list.append(pred_dist)
+
     assert margin_measure_confusion['tp'] + margin_measure_confusion['fn'] \
         == linear_search_confusion['tp'] + linear_search_confusion['fn'], 'there is a bug'
     assert margin_measure_confusion['tn'] + margin_measure_confusion['fp'] \
@@ -158,9 +166,12 @@ def evaluate_results(x, model, k, loss_param):
         res['precision'] = tp / (tp + fp)
         res['f1-score'] = 2 * res['recall'] * res['precision'] / (res['recall'] + res['precision'])
         return res
+
     margin_res = get_scores(margin_measure_confusion)
     linear_search_res = get_scores(linear_search_confusion)
-    return (margin_res, margin_measure_confusion), (linear_search_res, linear_search_confusion), (pred_dist)
+    if cache_dist:
+        pred_dist_list = torch.cat(pred_dist_list, dim=0)
+    return (margin_res, margin_measure_confusion), (linear_search_res, linear_search_confusion), (pred_dist_list,)
 
 
 class TriMarginLoss:
