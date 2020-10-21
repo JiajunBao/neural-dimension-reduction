c5762202767849d11a370e89dca99df7fdf2b7b5
Jiajun <jiajunb@umich.edu>
2020-10-21 09:24:42 -0400

    add dropout on the original code

--- references/Thesis_Data_Code/testwithdropout.py
+++ references/Thesis_Data_Code/testwithdropout.py
@@ -4,7 +4,7 @@ from main import main
 kwargs_list1 = [
     {'input-size': 90000, 'test-size': 1000, 'hidden-dims': [500, 100, 20, 20, 20], 'output-dimension': 10,
      'lambda': 1, 'epochs': 20,
-     'to_plot': True, 'learning_rate': 0.01, 'momentum': .9, 'data': 'artificial_with_drop_out',
+     'to_plot': True, 'learning_rate': 0.01, 'momentum': .9, 'data': 'artificial3_10_200',
      'filename_prefix': 'withdropout'}
     ]
 main(kwargs_list1)
